a4f6339a603598d88eec25ce5621a841b33f8a60
Chris Lee <chrislee910320@gmail.com>
2020-07-19 00:17:00 -0600

    Updates

---
+++ precache-manifest.840facdf7ded3089b7096eb1792c6e56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0bca68a21363493b90e4bba995b37c",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "06374f892d24c2808587",
+    "url": "/QuizApp/static/js/2.dc1563bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuizApp/static/js/2.dc1563bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920a4788735f05b26560",
+    "url": "/QuizApp/static/js/main.20840d8b.chunk.js"
+  },
+  {
+    "revision": "a7f8694b575bd5cc29bc",
+    "url": "/QuizApp/static/js/runtime-main.7d9ee554.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/QuizApp/static/media/nattu-adnan.c11f97a3.jpg"
+  }
+]);
